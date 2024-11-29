341bbc91f09a664ad936c8a120c08dba1beaeb84
AnitaOwen <anitaowen@pursuit.org>
2024-11-29 15:34:48 -0500

    updated get all transactions by id query to sort transaction by date desc

--- queries/transactions.js
+++ queries/transactions.js
@@ -4,7 +4,7 @@ const db = require("../db/dbConfig");
 
 const getTransactionsByUserId = async (user_id) => {
     try {
-        const allTransactions = await db.any(`SELECT * FROM transactions WHERE user_id = $1`, user_id)
+        const allTransactions = await db.any(`SELECT * FROM transactions WHERE user_id = $1 ORDER BY date DESC`, user_id)
         return allTransactions
     } catch(error){
         console.error("Error retreiving transactions", error)
