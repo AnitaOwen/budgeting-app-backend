022a1568961729d587da67ec3aa7ad38cad53598
AnitaOwen <anitaowen@pursuit.org>
2024-11-30 21:37:50 -0500

    removed email test file

--- utils/emailTest.js
+++
@@ -1,6 +0,0 @@
-const { sendVerificationEmail } = require("../utils/email");
-
-// Test the function
-sendVerificationEmail("aveniia@gmail.com", "test-token")
-  .then(() => console.log("Email sent successfully!"))
-  .catch((error) => console.error("Error sending email:", error));
